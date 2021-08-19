dc63e71371bc9e8be8780ec2c071b22eb35af2a8
Tulio Ferreira <tuliof210@gmail.com>
2021-08-19 17:09:54 -0300

    fix: interceptors imports

--- src/interceptors/Logger.interceptor.ts
+++ src/interceptors/Logger.interceptor.ts
@@ -1,8 +1,7 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-
-import { Request, Response } from 'express';
 
 @Injectable()
 export class LoggerInterceptor implements NestInterceptor {
