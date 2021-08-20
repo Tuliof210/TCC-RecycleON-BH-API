4a52835958c185ae3e3cf0367482e41db754648e
Tulio Ferreira <tuliof210@gmail.com>
2021-08-20 19:50:39 -0300

    feat: import all user useCases

--- src/useCases/user/User.module.ts
+++ src/useCases/user/User.module.ts
@@ -1,9 +1,9 @@
 import { CreateUserController, CreateUserService, CREATE_USER_SERVICE } from './create';
-import { DeleteUserController } from './delete';
-import { DisableUserController } from './disable';
+import { DeleteUserController, DeleteUserService, DELETE_USER_SERVICE } from './delete';
+import { DisableUserController, DisableUserService, DISABLE_USER_SERVICE } from './disable';
 import { GetUserController, GetUserService, GET_USER_SERVICE } from './get';
 import { RetrieveUsersController, RetrieveUsersService, RETRIEVE_USERS_SERVICE } from './retrieve';
-import { UpdateUserController } from './update';
+import { UpdateUserController, UpdateUserService, UPDATE_USER_SERVICE } from './update';
 
 import { ResponseHelper, RESPONSE_HELPER } from 'src/helpers';
 import { UserMongoDBRepositoryModule } from 'src/repositories/users/mongoDB';
@@ -23,8 +23,11 @@ import { Module } from '@nestjs/common';
   providers: [
     { provide: RESPONSE_HELPER, useClass: ResponseHelper },
     { provide: CREATE_USER_SERVICE, useClass: CreateUserService },
+    { provide: DELETE_USER_SERVICE, useClass: DeleteUserService },
+    { provide: DISABLE_USER_SERVICE, useClass: DisableUserService },
     { provide: GET_USER_SERVICE, useClass: GetUserService },
     { provide: RETRIEVE_USERS_SERVICE, useClass: RetrieveUsersService },
+    { provide: UPDATE_USER_SERVICE, useClass: UpdateUserService },
   ],
 })
 export class UserModule {}
