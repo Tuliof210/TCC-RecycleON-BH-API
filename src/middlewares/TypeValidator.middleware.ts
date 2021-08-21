9f7ee6f954ad1e1783060013b1948f8ff761f389
Tulio Ferreira <tuliof210@gmail.com>
2021-08-21 00:58:02 -0300

    feat: bad promise middleware

--- src/middlewares/TypeValidator.middleware.ts
+++ src/middlewares/TypeValidator.middleware.ts
@@ -1,61 +1,61 @@
 import { CreateUserDTO, UpdateUserDTO } from 'src/DTO';
+import { ResponseHelper } from 'src/helpers';
 
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
 
 import { Request, Response, NextFunction } from 'express';
 import * as yup from 'yup';
 
 @Injectable()
 export class TypeValidationMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log(this, '14');
+  private responseHelper: ResponseHelper;
+  private responseShortcut: Response;
+  private readonly coverageMap = {
+    '/users': { POST: 'usersPostValidation', PUT: 'userPutValidation' },
+  };
 
+  constructor() {
+    this.responseHelper = new ResponseHelper();
+  }
+
+  use(req: Request, res: Response, next: NextFunction) {
+    this.responseShortcut = res;
     const { body, baseUrl, method } = req;
     if (req.body) req.body = this.requestBodyHandler(body, baseUrl, method);
 
     next();
   }
 
-  private readonly coverageMap = {
-    '/users': { POST: this.usersPostValidation, PUT: this.userPutValidation },
-  };
-
-  private requestBodyHandler(body: any, baseUrl: string, method: string) {
-    const handler = this.coverageMap[baseUrl][method];
-    return handler ? handler(body) : body;
+  private executeValidation<Type>(body: any, schema: yup.SchemaOf<Type>) {
+    return schema
+      .validate(body)
+      .then(() => schema.cast(body, { stripUnknown: true }))
+      .catch((err) => {
+        console.log(err);
+        console.log(err.errors);
+        //throw { error: 'a', message: e.errors[0] };
+        return this.responseHelper.failure(this.responseShortcut, HttpStatus.BAD_REQUEST)(err);
+      });
   }
 
-  //TODO fix hardcoded yup
+  private requestBodyHandler(body: any, baseUrl: string, method: string) {
+    const handler = this.coverageMap[baseUrl][method] as string;
+    return handler && this[handler] ? this[handler](body) : body;
+  }
+
   private usersPostValidation(body: any) {
     const schema: yup.SchemaOf<CreateUserDTO> = yup.object({
       name: yup.string().required(),
       email: yup.string().email().required(),
       password: yup.string().min(8).required(),
     });
-
-    console.log(this, '35');
-
-    return this.executeValidation(body, schema);
+    return this.executeValidation<CreateUserDTO>(body, schema);
   }
 
-  //TODO fix hardcoded yup
   private userPutValidation(body: any) {
     const schema: yup.SchemaOf<UpdateUserDTO> = yup.object({
       name: yup.string().required(),
     });
-
-    return this.executeValidation(body, schema);
-  }
-
-  private executeValidation(body: any, schema) {
-    console.log('hiii');
-
-    try {
-      schema.validateSync(body);
-      schema.cast(body, { stripUnknown: true });
-    } catch (error) {
-      console.log(error);
-      throw { name: 'a' };
-    }
+    return this.executeValidation<UpdateUserDTO>(body, schema);
   }
 }
