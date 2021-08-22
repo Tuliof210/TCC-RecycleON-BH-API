e07784d0dd0a6523189a1566d1bcb5f67694b260
Tulio Ferreira <tuliof210@gmail.com>
2021-08-22 16:26:41 -0300

    feat: create user using httpstatus

--- src/useCases/user/create/CreateUser.controller.ts
+++ src/useCases/user/create/CreateUser.controller.ts
@@ -2,7 +2,7 @@ import { CreateUserDTO, UserViewDTO } from 'src/DTO';
 import { ICreateUserController, ICreateUserService, CREATE_USER_SERVICE, CreateUserValidationPipe } from '.';
 import { StandardSuccess, StandardError } from 'src/classes';
 
-import { Body, Controller, Inject, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Inject, Post, UsePipes } from '@nestjs/common';
 
 @Controller('users')
 export class CreateUserController implements ICreateUserController {
@@ -13,7 +13,7 @@ export class CreateUserController implements ICreateUserController {
   async handle(@Body() userData: CreateUserDTO): Promise<StandardSuccess<UserViewDTO>> {
     try {
       const createdUser = await this.createUserService.execute(userData);
-      return new StandardSuccess<UserViewDTO>(createdUser, 201);
+      return new StandardSuccess<UserViewDTO>(createdUser, HttpStatus.CREATED);
     } catch (e) {
       throw new StandardError(e, e.statusCode);
     }
