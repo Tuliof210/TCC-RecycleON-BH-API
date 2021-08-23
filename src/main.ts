8233e2ad7e8e23e56d8b2703b920da6d4703d094
Tulio Ferreira <tuliof210@gmail.com>
2021-08-23 19:27:11 -0300

    feat: add security

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { AppModule } from './App.module';
 import { NestFactory } from '@nestjs/core';
 
 import * as compression from 'compression';
+import * as csurf from 'csurf';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   app.use(compression());
+  app.use(csurf());
+  app.use(helmet());
 
   await app.listen(3070);
 }
