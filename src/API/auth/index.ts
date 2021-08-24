24c71b11f8d7038ad5eab23ad866bcf3f9dadba9
Tulio Ferreira <tuliof210@gmail.com>
2021-08-24 20:19:37 -0300

    fix: remove login pipe and DTO

--- src/API/auth/index.ts
+++ src/API/auth/index.ts
@@ -1,13 +1,11 @@
-import { CreateUserDTO, LoginDTO, UserViewDTO } from 'src/DTO';
+import { UserViewDTO } from 'src/DTO';
 
 export interface IAuthController {
   login({ user }: { user: UserViewDTO }): { token: string; user: UserViewDTO };
-  //signup(userData: CreateUserDTO): Promise<{ token: string; user: UserViewDTO }>;
 }
 
 export interface IAuthService {
   validateUser(email: string, password: string): Promise<void | UserViewDTO>;
-  //signup(userData: CreateUserDTO): Promise<{ token: string; user: UserViewDTO }>;
 }
 
 export { AuthModule } from './Auth.module';
--- src/pipes/index.ts
+++ src/pipes/index.ts
@@ -1,3 +1,2 @@
 export { CreateUserValidationPipe } from './CreateUserValidation.pipe';
-export { LoginValidationPipe } from './LoginValidation.pipe';
 export { UpdateUserValidationPipe } from './UpdateUserValidation.pipe';
