65674cca0b9bae1459768b6f310cb2715a4538ec
Tulio Ferreira <tuliof210@gmail.com>
2021-08-25 18:36:04 -0300

    feat: add passport http

---
+++ src/guards/Basic.strategy.ts
@@ -0,0 +1,7 @@
+import { AuthPayloadDTO } from 'src/shared/DTO';
+import { IAuthService } from 'src/API/auth';
+
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+
+import {} from '';
