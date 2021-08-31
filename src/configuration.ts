111a0f115abf5bdd02741c6b1e93e3000074792d
Tulio Ferreira <tuliof210@gmail.com>
2021-08-31 20:44:38 -0300

    fix: port

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,6 +1,5 @@
 export default () => ({
   environment: process.env.ENVIRONMENT || 'development',
-  port: parseInt(process.env.HOST_PORT, 10) || 3070,
   database: process.env.DATABASE_HOST,
   secretkeys: {
     jwt: process.env.JWT_SECRET,
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,17 @@
 import { AppModule } from './App.module';
 
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
 
 import * as compression from 'compression';
 import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('port');
 
   app.enableCors();
   app.use(helmet());
   app.use(compression());
 
-  await app.listen(port);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
