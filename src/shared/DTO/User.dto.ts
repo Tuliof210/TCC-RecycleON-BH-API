644e7bc5b1c1fa17a3ebde0da8a9185df4abd246
Tulio Ferreira <tuliof210@gmail.com>
2021-10-20 12:17:06 -0300

    feat: update user update

--- src/shared/DTO/User.dto.ts
+++ src/shared/DTO/User.dto.ts
@@ -2,7 +2,7 @@ import { User } from 'src/shared/entities';
 import { Document } from 'mongoose';
 
 export type CreateUserDTO = Pick<User, 'name' | 'email' | 'password'>;
-export type UpdateUserDTO = Partial<Pick<User, 'name' | 'email' | 'role'>>;
+export type UpdateUserDTO = Partial<Pick<User, 'name' | 'email' | 'role' | 'password'>>;
 export interface UserDTO extends Partial<User> {
   _id?: any;
   authenticate?: (password: string) => Promise<void | UserDTO>;
