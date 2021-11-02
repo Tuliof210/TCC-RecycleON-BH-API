d1e536e64213e90b5fb0b7f0a74a450ed3dd0c8c
Tulio Ferreira <tuliof210@gmail.com>
2021-11-02 12:46:55 -0300

    fix: index of wiki mongodb

--- src/repositories/wiki/index.ts
+++ src/repositories/wiki/index.ts
@@ -1,9 +1,9 @@
-import { Wiki } from 'src/shared/entities';
-import { WikiDTO } from 'src/shared/DTO';
+import { WikiItem } from 'src/shared/entities';
+import { WikiItemDTO } from 'src/shared/DTO';
 
 export interface IWikiRepository {
-  saveOrUpdate(wiki: Wiki): Promise<WikiDTO>;
-  getById(_id: string): Promise<WikiDTO>;
-  retrieveAll(): Promise<{ count: number; list: Array<WikiDTO> }>;
+  saveOrUpdate(wikiItem: WikiItem): Promise<WikiItemDTO>;
+  getById(_id: string): Promise<WikiItemDTO>;
+  retrieveAll(): Promise<{ count: number; list: Array<WikiItemDTO> }>;
 }
 export const IWikiRepositoryToken = 'IWikiRepositoryToken';
